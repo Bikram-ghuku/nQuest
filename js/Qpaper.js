0ae9ffad609b345fe3e3b758bd6129368e160ff3
Bikram-ghuku <bikramghuku05@gmail.com>
2023-06-06 09:19:00 +0530

    ui developed

---
+++ js/Qpaper.js
@@ -0,0 +1,38 @@
+const qpaper= {
+    id: "test_pap_1",
+    data:
+    [{
+        group: "Physics",
+        gid: "0",
+        sections:[{
+            section: "Physics Section 1",
+            id: "py_sec_1",
+            num_ques: 5
+        },
+        {
+            section: "Physics Section 2",
+            id: "py_sec_2",
+            num_ques: 5
+
+        }]
+
+    },
+    {
+        group: "Chemistry",
+        gid: "1",
+        sections:[{
+            section: "Chemistry Section 1",
+            id: "chem_sec_1",
+            num_ques: 5
+        },
+        {
+            section: "Chemistry Section 2",
+            id: "chem_sec_2",
+            num_ques: 5
+
+        }]
+
+
+    }
+]
+}
