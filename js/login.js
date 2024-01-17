a9c24c8d4fb8fd7d8f729be04648aa4f3c31565c
Bikram Ghuku <bikramghuku05@gmail.com>
2024-01-17 22:17:06 +0530

    fix: show login screen on socket msg

--- js/login.js
+++ js/login.js
@@ -13,6 +13,18 @@ socket.on('connect', () => {
     socket.emit('system_online', {sys_no, lab_no, socket_id: socket.id})
 });
 
+socket.on('userData', (data) => {
+    console.log(data)
+    const x = data
+    document.getElementsByClassName("user_assigned")[0].style = "display:block"
+    document.getElementsByClassName("login_form")[0].style= "height: 70vh; padding: 10vh;"
+    document.getElementsByClassName("uname_contents")[0].innerHTML = x.user
+    document.getElementById("uname").value = x.id_no
+    document.getElementsByClassName("top")[0].innerHTML = x.exam_name
+	document.getElementsByClassName("sub_contents")[0].innerHTML = x.exam_name
+    $("#uname").prop("readonly", true);
+});
+
 document.getElementsByClassName("top")[0].innerHTML = ""
 if(!sys_no && !lab_no){
     localStorage.setItem("user_data", JSON.stringify(offuser))
@@ -31,14 +43,6 @@ if(!JSON.parse(localStorage.getItem("user_data"))){
 	document.getElementsByClassName("sub_contents")[0].innerHTML = x.exam_name
     $("#uname").prop("readonly", true);
 }
+
 document.getElementsByClassName("lab_contents")[0].innerHTML = lab_no
 document.getElementsByClassName("sys_contents")[0].innerHTML = sys_no
-
-setInterval(()=>{
-    if(!localStorage.getItem("user_data")){
-        // location.reload()
-        
-    }
-
-
-}, 5000)
