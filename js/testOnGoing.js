563acacb8f792b4df59cd20e6fc6261994798f12
Bikram Ghuku <bikramghuku05@gmail.com>
2024-01-17 23:02:24 +0530

    fix: connect and send status update

--- js/testOnGoing.js
+++ js/testOnGoing.js
@@ -1,4 +1,10 @@
+const socket = io.connect('http://localhost:3000');
 
+socket.on('connect', () => {
+    console.log("Connected", socket.id)
+    socket.emit('system_online', {sys_no : x.system_id, lab_no: x.lab_no, socket_id: socket.id, type: 'examSys'})
+    socket.emit('status', {sys_no : x.sys_no, lab_no: x.lab_no, socket_id: socket.id, type: 'examSys', status: 'examStart'});
+});
 
 var qpaper = JSON.parse(localStorage.getItem("qpaper"))
 var groups = qpaper.data
